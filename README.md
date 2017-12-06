09840dcf0d6cd0bb3ddf14faa7bb163f9d5d7e49
Sönke <me@eknoes.de>
2017-12-06 19:56:17 +0100

    Added Readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# ip-map
-Create a Heatmap out of failed SSH Logins with Python. Heavily inspired by [/u/Ultradad](https://www.reddit.com/r/dataisbeautiful/comments/7gvm5p/heatmap_of_attempted_ssh_logins_on_my_server_oc/)
+# IP-Map
+Creates out of failed SSH Logins with Python. Heavily inspired by [/u/Ultradad](https://www.reddit.com/r/dataisbeautiful/comments/7gvm5p/heatmap_of_attempted_ssh_logins_on_my_server_oc/)
+
+![Example of Failed SSH Login Attempts Graphics][example]
+
+# Requirements
+It relies on [matplotlib] and [cartopy], so its quite a hassle to install properly - Sorry!
+
+Furthermore, you need a CSV file which maps IPv4 subnetworks to geolocations, like they come from [maxmind] ("GeoLite2 City"). The only important thing is, that it has the fields `network,latitude,longitude`. The default filename is `geolocation.csv`, but that can be changed with the `--geolocation-csv` parameter.
+
+# Usage
+On your Server, you need to read the `auth.log` files:
+```bash
+grep "Failed" /var/log/auth.log | grep -Po "[\d]+\.[\d]+\.[\d]+\.[\d]+" | sort | uniq -c > access_failed.csv
+```
+
+As most of the computing time is used for the matching of IPs to a subnetwork, this result can be cached with the `--cache-locations locations.csv` parameter. So if the dataset did not change, it can be run with the `--read-cache locations.csv` parameter to directly start with drawing the map.
+
+```text
+usage: create.py [-h] [--cache-locations WRITE_LOCATIONS_FILE]
+                 [--read-cache READ_LOCATIONS_FILE]
+                 [--result-filename RESULT_FILENAME]
+                 [--geolocation-csv GEOLOCATION_FILENAME]
+                 input_file
+
+Create a Worldmap out of Numbers and IPv4 addresses
+
+positional arguments:
+  input_file            File with Number of attempts followed by IPv4 address,
+                        one per line
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --cache-locations WRITE_LOCATIONS_FILE
+                        Cache predicted locations in a file
+  --read-cache READ_LOCATIONS_FILE
+                        Read location data from cache rather then computing it
+  --result-filename RESULT_FILENAME
+                        Filename of the resulting map
+  --geolocation-csv GEOLOCATION_FILENAME
+                        CSV file with geolocation / network data
+
+```
+
+[example]: examples/result.png
+[matplotlib]: http://matplotlib.org
+[cartopy]: http://scitools.org.uk/cartopy/
+[maxmind]: https://dev.maxmind.com/geoip/geoip2/geolite2/
--- create.py
+++ create.py
@@ -1,26 +1,43 @@
 import csv
 import ipaddress
 from time import time
-
-# Ma
 import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import math
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a Worldmap out of Numbers and IPv4 addresses')
+parser.add_argument('input_file', metavar='input_file', type=str,
+                    help='File with Number of attempts followed by IPv4 address, one per line')
+parser.add_argument('--cache-locations', dest='write_locations_file', type=str,
+                    help='Cache predicted locations in a file')
+parser.add_argument('--read-cache', dest='read_locations_file', type=str,
+                    help='Read location data from cache rather then computing it')
+parser.add_argument('--result-filename', dest='result_filename', type=str, default='result.png',
+                    help='Filename of the resulting map')
+parser.add_argument('--geolocation-csv', dest='geolocation_filename', type=str, default='geolocation.csv',
+                    help='CSV file with geolocation / network data')
+
+args = parser.parse_args()
 
 failed_logins = []
 subnet_locations = []
 results = []
 
-def prepare_data():
+# Step 1: Preparing means reading and parsing geolocation and failed logins file
+def prepare_data(input_filename, geolocation_filename):
     def read_tries():
         '''Converts output from: grep "Failed" /var/log/auth.log | grep -Po "[\d]+\.[\d]+\.[\d]+\.[\d]+" | sort | uniq -c'''
-        with open("access_failed.csv", newline='') as csvfile:
+        with open(input_filename, newline='') as csvfile:
             access_reader = csv.reader(csvfile, delimiter=' ', strict=True, skipinitialspace=True)
             for row in access_reader:
                 binary_ip = int(ipaddress.ip_address(row[1]))
                 failed_logins.append((binary_ip, row[0]))
 
     def init_geolocation():
-        with open("geolocation.csv", newline='') as csvfile:
+        with open(geolocation_filename, newline='') as csvfile:
             geoloc_reader = csv.DictReader(csvfile)
             for line in geoloc_reader:
                 subnet_locations.append((line['network'], line['latitude'], line['longitude']))
@@ -44,7 +61,7 @@ def analyze():
         for ip in failed_logins:
             if (ip[0] & mask) == netw:
                 results.append((ip[1], subnet[1], subnet[2]))
-                print("Found location for " + str(ip[0]))
+                failed_logins.remove(ip)
 
     print('Start Location Search')
     t1 = time()
@@ -57,16 +74,55 @@ def analyze():
     t2 = time()
     print('Finding Location took %f seconds' % round(t2-t1, 2))
 
-def draw_map():
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.coastlines()
-    ax.stock_img()
+def write_locations():
+    with open("locations.csv", "w") as tempfile:
+        locationwriter = csv.writer(tempfile)
+        for line in results:
+            locationwriter.writerow(line)
+
+def load_results():
+    with open("locations.csv") as tempfile:
+        locationreader = csv.reader(tempfile)
+        for row in locationreader:
+            results.append(row)
+
+def draw_map(filename):
+    print('Start Draw Map')
+    t1 = time()
+
+    ax = plt.axes(projection=ccrs.Robinson())
+    countries_shp = shpreader.natural_earth(resolution='10m',
+                                        category='cultural', name='admin_0_countries')
+
+    for country in shpreader.Reader(countries_shp).records():
+        ax.add_geometries(country.geometry, ccrs.PlateCarree(),
+                  facecolor='#666666')
+
+    max_failed = max(int(line[0]) for line in results)
+
+    colormap = mpl.cm.Reds
+
     for location in results:
-        plt.plot(float(location[1]), float(location[2]), color='red', marker='o',transform=ccrs.PlateCarree())
-    plt.show()
+        plt.plot(float(location[2]), float(location[1]), color=colormap(math.log(float(location[0])) / math.log(max_failed)), marker='.', alpha=0.8, fillstyle='full',markeredgewidth=0,transform=ccrs.PlateCarree(),markersize=math.log(int(location[0])))
 
-prepare_data()
-analyze()
-draw_map()
+    # Legend
+    sm = plt.cm.ScalarMappable(cmap=colormap,norm=mpl.colors.LogNorm(1,max_failed))
+    sm._A = []
+    colorbar = plt.colorbar(sm,ax=ax,ticks=[10**i for i in range(0, round(math.log10(max_failed)))]+[max_failed], format='%.0f')
+    colorbar.ax.set_ylabel('Number of Attempts', rotation=90)
+    plt.title("Failed SSH Logins by Location and #Attempts")
+    plt.tight_layout()
+    plt.savefig(filename, dpi=1000)
+    t2 = time()
+    print('Drawing map took %f seconds' % round(t2-t1, 2))
 
-print("Found %i locations" % len(results))
+if __name__ == "__main__":
+    if not args.read_locations_file:
+        prepare_data(args.input_file, args.geolocation_filename)
+        analyze()
+        if args.write_locations_file:
+            write_locations(args.write_locations_file)
+    else:
+        load_results(args.read_locations_file)
+
+    draw_map(args.result_filename)
